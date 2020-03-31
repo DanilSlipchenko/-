5ed1edd734f9ac2e8f6ca45479fb76e3d18fdb5f
DanilSlipchenko <62115079+DanilSlipchenko@users.noreply.github.com>
2020-03-31 22:55:55 +0400

    Salam

---
+++ unik/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    bool up;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < 2 && !up)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime);
+            transform.Rotate (Vector3.up * 50 * Time.deltaTime);
+            if(transform.position.y >= 1.5f)
+            up = true;
+        }  
+
+        else if(transform.position.y > 0 && up)
+        {
+            transform.Translate(Vector3.up * -1 * Time.deltaTime);
+            transform.Rotate (Vector3.up * -50 * Time.deltaTime);
+            if(transform.position.y <= 0)
+            up = false; 
+        }
+    }
+}
---
+++ unik/Assets/Scripts/NewBehaviourScript1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool up;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < 1.5f && !up)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime);
+            transform.Rotate (Vector3.up * 900 * Time.deltaTime);
+            if(transform.position.y >= 1.5f)
+            up = true;
+        }  
+
+        else if (transform.position.y > 0 && up)
+        {
+            transform.Translate(Vector3.up * -1 * Time.deltaTime);
+            transform.Rotate (Vector3.up * -900 * Time.deltaTime);
+            if(transform.position.y <= 0)
+            up = false; 
+        }
+    }
+}
